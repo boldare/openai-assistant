d63c8459af40c858294ef700688a6a93147ae5ac
Sebastian Musiał <sebastian.musial@boldare.com>
2023-11-27 11:05:33 +0100

    feat(app): env config and removed unused default NestJS code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot()],
 })
 export class AppModule {}

6cbf7464a628221d874bd8466d3547b153100e2d
Sebastian Musiał <kontakt@sebastianmusial.pl>
2023-11-28 08:35:24 +0100

    feat(assistant): created basic functionality for assistant service (#3)
    * feat(assistant): created assistant module with empty services
    * feat(app): env config and removed unused default NestJS code
    * feat(assistant): created basic functionality for assistant service
    * feat(assistant): saved the assistant ID in the env variables

--- src/assistant/assistant.service.ts
+++ src/assistant/assistant.service.ts
@@ -1,4 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { Assistant } from 'openai/resources/beta';
+import { AiService } from './ai.service';
+import { AssistantConfig } from './assistant.model';
+import { writeFile, readFile } from 'fs/promises';
+import * as envfile from 'envfile';
 
 @Injectable()
-export class AssistantService {}
+export class AssistantService implements OnModuleInit {
+  private readonly logger = new Logger(AssistantService.name);
+  private readonly assistants = this.aiService.openai.beta.assistants;
+  public assistant: Assistant;
+
+  constructor(
+    @Inject('config') private config: AssistantConfig,
+    private readonly aiService: AiService,
+  ) {}
+
+  async onModuleInit(): Promise<void> {
+    this.assistant = await this.init();
+  }
+
+  async init(): Promise<Assistant> {
+    const assistantId = this.config.id;
+
+    if (!assistantId) {
+      return this.create();
+    }
+
+    try {
+      // @TODO: Retrieve and edit assistant API
+      return await this.assistants.retrieve(assistantId);
+    } catch (e) {
+      return this.create();
+    }
+  }
+
+  async create(): Promise<Assistant> {
+    const assistant = await this.assistants.create(
+      this.config.params,
+      this.config.options,
+    );
+
+    this.logger.log(`Created new assistant (${assistant.id})`);
+    await this.saveAssistantId(assistant.id);
+
+    return assistant;
+  }
+
+  async saveAssistantId(id: string): Promise<void> {
+    try {
+      const sourcePath = './.env';
+      const envVariables = await readFile(sourcePath);
+      const parsedVariables = envfile.parse(envVariables.toString());
+      const newVariables = {
+        ...parsedVariables,
+        ASSISTANT_ID: id,
+      };
+
+      await writeFile(sourcePath, envfile.stringify(newVariables));
+    } catch (error) {
+      this.logger.error(`Can't save save variable: ${error}`);
+    }
+  }
+}
--- src/assistant/chatbot.service.ts
+++ src/assistant/chatbot.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import OpenAI from 'openai';
+import { Assistant } from 'openai/resources/beta';
+import { AiService } from './ai.service';
 
 @Injectable()
-export class ChatbotService {}
+export class ChatbotService {
+  provider: OpenAI = this.aiService.openai;
+  assistant: Assistant;
+
+  constructor(private aiService: AiService) {}
+}
